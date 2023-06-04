d460e5ef71a8795af3c8888c83474c91e7d0febb
romalitv <youlikeyoupass@gmail.com>
2023-06-04 14:03:01 +0200

    Matrix for alpine image

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,8 +1,19 @@
 from fastapi import APIRouter
+import numpy
 
 router = APIRouter()
 
+@router.get('/matrix')
+def matrix() -> dict:
+    
+    matrix_1 = numpy.random.rand(10, 10)
+    matrix_2 = numpy.random.rand(10, 10)
+    product = numpy.dot(matrix_1, matrix_2)
 
-@router.get('')
-def hello_world() -> dict:
-    return {'msg': 'Hello, World!'}
+    res = {
+        "matrix_a": matrix_1.tolist(),
+        "matrix_b": matrix_2.tolist(),
+        "product": product.tolist()
+    }
+
+    return res
